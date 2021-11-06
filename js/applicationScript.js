4ed33af4e99c761daf827fade4f0c2ae245d0a01
CAEUser <mail@rwth>
2021-11-06 14:41:10 +0000

    ApplicationScript edited by Anonymous

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -32,7 +32,7 @@
 
 var client;
 
-var init = function() {loadSong()
+var init = function() {
   var iwcCallback = function(intent) {
     // define your reactions on incoming iwc events here 
     console.log(intent);
